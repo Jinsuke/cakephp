d342c5511ea737f09aeb50dfb263f3a1018d5b60
unknown <jerome.gokapi@MNL-MYREL.corelcorp.corel.ics>
2015-01-05 11:02:42 +0800

    Initial Commit

---
+++ app/Controller/PostsController.php
@@ -0,0 +1,90 @@
+<?php
+
+
+class PostsController extends AppController {
+    public $helpers = array('Html', 'Form', 'Session');
+    public $components = array('Session');
+
+    public function index() {
+        $this->set('posts', $this->Post->find('all'));
+    }
+
+    public function view($id) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        $post = $this->Post->findById($id);
+        if (!$post) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+        $this->set('post', $post);
+    }
+
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->request->data['Post']['user_id'] = $this->Auth->user('id');
+            $this->Post->create();            
+            if ($this->Post->save($this->request->data)) {
+                $this->Session->setFlash(__('Your post has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Session->setFlash(__('Unable to add your post.'));
+        }
+}
+  
+    public function edit($id = null) {
+    if (!$id) {
+        throw new NotFoundException(__('Invalid post'));
+    }
+
+    $post = $this->Post->findById($id);
+    if (!$post) {
+        throw new NotFoundException(__('Invalid post'));
+    }
+
+    if ($this->request->is(array('post', 'put'))) {
+        $this->Post->id = $id;
+        if ($this->Post->save($this->request->data)) {
+            $this->Session->setFlash(__('Your post has been updated.'));
+            return $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('Unable to update your post.'));
+    }
+
+    if (!$this->request->data) {
+        $this->request->data = $post;
+    }
+    }
+
+    public function delete($id) {
+    if ($this->request->is('get')) {
+        throw new MethodNotAllowedException();
+    }
+
+    if ($this->Post->delete($id)) {
+        $this->Session->setFlash(
+            __('The post with id: %s has been deleted.', h($id))
+        );
+        return $this->redirect(array('action' => 'index'));
+    }
+    }
+
+    public function isAuthorized($user) {
+    // All registered users can add posts
+    if ($this->action === 'add') {
+        return true;
+    }
+
+    // The owner of a post can edit and delete it
+    if (in_array($this->action, array('edit', 'delete'))) {
+        $postId = (int) $this->request->params['pass'][0];
+        if ($this->Post->isOwnedBy($postId, $user['id'])) {
+            return true;
+        }
+    }
+
+    return parent::isAuthorized($user);
+    }
+
+}
---
+++ app/Model/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+class Post extends AppModel {
+    public $validate = array(
+        'title' => array(
+            'rule' => 'notEmpty'
+        ),
+        'body' => array(
+            'rule' => 'notEmpty'
+        )
+    );
+
+    public function isOwnedBy($post, $user) {
+    return $this->field('id', array('id' => $post, 'user_id' => $user)) !== false;
+	}
+	
+}
